142c5cc34d94ed30356fd9809d39254325c5857f
danpilnet <dnpdnp28@gmail.com>
2024-04-29 01:11:14 +0900

    task_2

--- NewsPortal/NewsPortal/celery.py
+++ NewsPortal/NewsPortal/celery.py
@@ -1,5 +1,5 @@
 import os
-
+from celery.schedules import crontab
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPortal.settings')
@@ -8,3 +8,10 @@ app = Celery('NewsPortal')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'action_every_monday_8am': {
+        'task': 'action',
+        'schedule': crontab(),
+    },
+}
