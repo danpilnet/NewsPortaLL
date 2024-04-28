3df9dda457f0283fcfaba2584ff1467ea7ed3b9a
danpilnet <dnpdnp28@gmail.com>
2024-04-28 23:04:49 +0900

    tasks

--- NewsPortal/news/tasks.py
+++ NewsPortal/news/tasks.py
@@ -2,6 +2,8 @@ from celery import shared_task
 from django.template.loader import render_to_string
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
+from celery.schedules import crontab
+
 
 
 
@@ -27,3 +29,16 @@ def sending(preview, pk, title, subscribers):
     )
 
 
+    message_tasks.attach_alternative(html_content, 'text/html')
+    message_tasks.send()
+
+
+
+
+@shared_task()
+app.conf.beat_schedule = {
+    'action_every_monday_8am': {
+        'task': 'action',
+        'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
+    },
+}
